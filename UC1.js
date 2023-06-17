5d1dbe69f49b35ff8942227330fd96b4a59c6206
Vrushali Joshi <vrushalijoshi589@gmail.com>
2023-06-17 13:24:20 +0530

    check UC1 employee is present or absent

---
+++ UC1.js
@@ -0,0 +1,10 @@
+console.log("Welcome to Employee Wage camputation")
+let num=Math.floor(Math.random()*2);
+if(num==0)
+{
+    console.log("Employee is Absent")
+}
+else
+{
+    console.log("Employee is Present");
+}
